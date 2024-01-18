d2bc089f45b1e1abbe9f4ddec29ae8f119949002
yjkwak <silverlnng0721@gmail.com>
2024-01-18 20:33:17 +0900

    주석추가

--- Source/NetWorkProject1/Private/NetworkGameModeBase.cpp
+++ Source/NetWorkProject1/Private/NetworkGameModeBase.cpp
@@ -5,7 +5,8 @@
 #include "EngineUtils.h"
 #include "GameFramework/PlayerStart.h"
 
-//아무조건이 없다면 함수호출되는순서 => 들어오는 순서
+// 레벨이 변경 => 모두 새롭게 재입장 !! (세션에 한번입장 개념이 아님 ) 
+// 함수호출 => 플레이어 입장할때 마다 
 // AController* player  을 인자로 받아서 플레이어에 접근을 해서 플레이어의 기준으로 설정도 가능하다 !
 AActor* ANetworkGameModeBase::ChoosePlayerStart_Implementation(AController* player)
 {
